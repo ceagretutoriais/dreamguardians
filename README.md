b89bd506b819fc18490e4d67dc690be9413e552d
ceagretutoriais <iot.barragem@gmail.com>
2023-06-10 12:11:02 -0300

    first commit

---
+++ README.md
@@ -0,0 +1,36 @@
+<h1 align="center">Dreamguardians</h1>
+
+<p align="center">
+    <a href="https://youtube.com/shorts/7BEqIu12KAY?feature=share">
+        <img src="https://th.bing.com/th/id/R.fe0261c4c4c342056e1d60670e747614?rik=4fU3YGXUG6bpyQ&riu=http%3a%2f%2fwww.educacao.am.gov.br%2fwp-content%2fuploads%2f2013%2f06%2fFoto-Roberto-Carlos-3.jpg&ehk=jxlN%2bmjf8Q3G67eF0ursha%2f7wvT24lSMjEi%2blrh8n0Q%3d&risl=&pid=ImgRaw&r=0" alt="count-objects-crossing-line">
+    </a>
+</p>
+
+## 👋 hello
+
+
+## 💻 Install
+
+```bash
+# create python virtual environment
+python -m venv venv
+
+# activate the virtual environment
+source venv/bin/activate
+
+# install dependencies
+pip install -r requirements.txt
+```
+
+## 📸 Execute
+
+```bash
+python -m main
+
+```
+
+## 📸 Descricao do software
+
+```bash
+Esse sistema criado pela equipe Dreamguardians tem o propósito de monitorar em tempo real alunos em ambientes educacionais. Através de câmeras o algoritmo detecta se os alunos estão tendo comportamento normal ou de violência. As tecnologias utilizadas são: Roboflow, Google Colab, Pycharm e Yolov8.
+```
---
+++ main.py
@@ -0,0 +1,82 @@
+import cv2
+import argparse
+
+from ultralytics import YOLO
+import supervision as sv
+import numpy as np
+
+
+ZONE_POLYGON = np.array([
+    [0.2, 0],#largura esquerda superior,
+    [0.5, 0],
+
+    [0.5, 0],#posiçao numero
+    [0.2, 0]#largura esquerda inferior, altura inferior
+])
+
+
+def parse_arguments() -> argparse.Namespace:
+    parser = argparse.ArgumentParser(description="Dream Guardians")
+    parser.add_argument(
+        "--webcam-resolution", 
+        default=[1500, 720],
+        nargs=2, 
+        type=int
+    )
+    args = parser.parse_args()
+    return args
+
+
+def main():
+    args = parse_arguments()
+    frame_width, frame_height = args.webcam_resolution
+
+    cap = cv2.VideoCapture(0)
+    cap.set(cv2.CAP_PROP_FRAME_WIDTH, frame_width)
+    cap.set(cv2.CAP_PROP_FRAME_HEIGHT, frame_height)
+
+    model = YOLO("escola_normal_violencia_v8.pt")
+
+    box_annotator = sv.BoxAnnotator(
+        thickness=2,
+        text_thickness=2,
+        text_scale=1
+    )
+
+    zone_polygon = (ZONE_POLYGON * np.array(args.webcam_resolution)).astype(int)
+    zone = sv.PolygonZone(polygon=zone_polygon, frame_resolution_wh=tuple(args.webcam_resolution))
+    zone_annotator = sv.PolygonZoneAnnotator(
+        zone=zone,
+        color=sv.Color.white(),
+        thickness=1,
+        text_thickness=1,
+        text_scale=1
+    )
+
+    while True:
+        ret, frame = cap.read()
+
+        result = model(frame, agnostic_nms=True)[0]
+        detections = sv.Detections.from_yolov8(result)
+        labels = [
+            f"{model.model.names[class_id]} {confidence:0.2f}"
+            for _, confidence, class_id, _
+            in detections
+        ]
+        frame = box_annotator.annotate(
+            scene=frame, 
+            detections=detections, 
+            labels=labels
+        )
+
+        zone.trigger(detections=detections)
+        frame = zone_annotator.annotate(scene=frame)      
+        
+        cv2.imshow("yolov8", frame)
+
+        if (cv2.waitKey(30) == 27):
+            break
+
+
+if __name__ == "__main__":
+    main()
